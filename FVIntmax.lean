3db29806ff13de671c48a973a6c553e1d6348009
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2024-08-28 11:42:31 +0200

    bump the file superstructure and do some red tape

--- FVIntmax.lean
+++ FVIntmax.lean
@@ -1,3 +1,4 @@
 import «FVIntmax».Wheels.AuthenticatedDictionary
 import «FVIntmax».Wheels.Dictionary
+import «FVIntmax».Wheels.SignatureAggregation
 import «FVIntmax».Wheels.Wheels
---
+++ FVIntmax/Wheels/SignatureAggregation.lean
@@ -0,0 +1,3 @@
+namespace Intmax
+
+end Intmax
