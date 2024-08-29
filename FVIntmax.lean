da24c4ae71b87f93fa18bd62820cff8208f2ab3b
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2024-08-29 09:16:19 +0200

    add file superstructure and red tape

--- FVIntmax.lean
+++ FVIntmax.lean
@@ -1,4 +1,5 @@
 import «FVIntmax».Wheels.AuthenticatedDictionary
 import «FVIntmax».Wheels.Dictionary
+import «FVIntmax».Wheels.Oracle
 import «FVIntmax».Wheels.SignatureAggregation
 import «FVIntmax».Wheels.Wheels
